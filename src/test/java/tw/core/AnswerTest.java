70dcb9614488ea46f26bc61d80bcb6fecfa356e6
Katrina <zhaoyanlin2016@gmail.com>
2018-04-22 12:05:15 +0900

    Implement unit test for getIndexOfNum method and toString method.

--- src/test/java/tw/core/AnswerTest.java
+++ src/test/java/tw/core/AnswerTest.java
@@ -1,8 +1,35 @@
 package tw.core;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * 在AnswerTest文件中完成Answer中对应的单元测试
  */
 public class AnswerTest {
+
+    List<String> answerList = Arrays.asList("1", "2", "3", "4");
+    private Answer answer;
+
+    @Before
+    public void setUp() throws Exception {
+        answer = new Answer();
+        answer.setNumList(answerList);
+    }
+
+    @Test
+    public void getIndexOfNumTest() {
+        assertEquals(answer.getIndexOfNum("3"), 2);
+    }
+
+    @Test
+    public void toStringTest() {
+        assertEquals(answer.toString(), "1 2 3 4");
+    }
 
 }
