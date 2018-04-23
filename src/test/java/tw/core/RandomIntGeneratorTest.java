28c824a6bbd0f2b9ad66b339325c7ad051a724ad
Katrina <zhaoyanlin2016@gmail.com>
2018-04-23 15:27:39 +0800

    Implement unit tests for RandomIntGenerator class.

--- src/test/java/tw/core/RandomIntGeneratorTest.java
+++ src/test/java/tw/core/RandomIntGeneratorTest.java
@@ -1,9 +1,44 @@
 package tw.core;
 
 
+import org.junit.Before;
+import org.junit.Test;
+import tw.core.generator.RandomIntGenerator;
+
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 /**
  * 在RandomIntGeneratorTest文件中完成RandomIntGenerator中对应的单元测试
  */
 public class RandomIntGeneratorTest {
 
+    @Before
+    public void setUp() throws Exception {
+        Random random = mock(Random.class);
+
+        when(random.nextInt()).thenReturn(1);
+        when(random.nextInt()).thenReturn(2);
+        when(random.nextInt()).thenReturn(3);
+        when(random.nextInt()).thenReturn(4);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void should_throw_IllegalArgumentException() {
+        RandomIntGenerator randomIntGenerator = new RandomIntGenerator();
+
+        randomIntGenerator.generateNums(3, 4);
+    }
+
+    @Test
+    public void should_generate_1234() {
+        RandomIntGenerator randomIntGenerator = new RandomIntGenerator();
+
+        String generatedAnswer = randomIntGenerator.generateNums(9, 4);
+
+        assertEquals(7, generatedAnswer.length());
+    }
 }
