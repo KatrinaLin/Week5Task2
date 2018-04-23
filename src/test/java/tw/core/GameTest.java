2b2a6f366cc99cb2ec08201cb753f9c85fa9dc97
Katrina <zhaoyanlin2016@gmail.com>
2018-04-23 19:04:49 +0800

    Use the static createAnswer method.

--- src/test/java/tw/core/GameTest.java
+++ src/test/java/tw/core/GameTest.java
@@ -26,8 +26,7 @@ public class GameTest {
     public void setUp() throws Exception {
         AnswerGenerator answerGenerator = mock(AnswerGenerator.class);
 
-        Answer correctAnswer = new Answer();
-        correctAnswer.setNumList(Arrays.asList("1", "2", "3", "4"));
+        Answer correctAnswer = Answer.createAnswer("1 2 3 4");
 
         when(answerGenerator.generate()).thenReturn(correctAnswer);
         
@@ -37,21 +36,14 @@ public class GameTest {
     @Test
     public void should_return_1A1B_when_guess() {
 
-        Answer guessAnswer = new Answer();
-        guessAnswer.setNumList(Arrays.asList("6", "2", "4", "0"));
-
-        GuessResult guessResult = game.guess(guessAnswer);
+        GuessResult guessResult = game.guess(Answer.createAnswer("6 2 4 0"));
 
         assertEquals("1A1B", guessResult.getResult());
     }
 
     @Test
     public void should_return_0A0B_when_guess() {
-
-        Answer guessAnswer = new Answer();
-        guessAnswer.setNumList(Arrays.asList("5", "6", "7", "8"));
-
-        GuessResult guessResult = game.guess(guessAnswer);
+        GuessResult guessResult = game.guess(Answer.createAnswer("5 6 7 8"));
 
         assertEquals("0A0B", guessResult.getResult());
     }
@@ -59,36 +51,21 @@ public class GameTest {
     @Test
     public void should_return_4A0B_when_guess_correct() {
 
-        Answer guessAnswer = new Answer();
-        guessAnswer.setNumList(Arrays.asList("1", "2", "3", "4"));
-
-        GuessResult guessResult = game.guess(guessAnswer);
+        GuessResult guessResult = game.guess(Answer.createAnswer("1 2 3 4"));
 
         assertEquals("4A0B", guessResult.getResult());
     }
 
     @Test
-    public void should_return_guessHistory_when_guess() {
+    public void should_return_guessHistory() {
 
-        Answer guessAnswer1 = new Answer();
-        guessAnswer1.setNumList(Arrays.asList("6", "2", "4", "0"));
+        game.guess(Answer.createAnswer("6 2 4 0"));
 
-        game.guess(guessAnswer1);
+        game.guess(Answer.createAnswer("9 2 5 4"));
 
-        Answer guessAnswer2 = new Answer();
-        guessAnswer2.setNumList(Arrays.asList("9", "2", "5", "4"));
+        game.guess(Answer.createAnswer("1 3 2 4"));
 
-        game.guess(guessAnswer2);
-
-        Answer guessAnswer3 = new Answer();
-        guessAnswer3.setNumList(Arrays.asList("1", "3", "2", "4"));
-
-        game.guess(guessAnswer3);
-
-        Answer guessAnswer4 = new Answer();
-        guessAnswer4.setNumList(Arrays.asList("1", "2", "3", "4"));
-
-        game.guess(guessAnswer4);
+        game.guess(Answer.createAnswer("1 2 3 4"));
 
         assertEquals("1A1B", game.guessHistory().get(0).getResult());
         assertEquals("2A0B", game.guessHistory().get(1).getResult());
@@ -99,26 +76,19 @@ public class GameTest {
     @Test
     public void should_return_status_success() {
 
-        Answer guessAnswer1 = new Answer();
-        guessAnswer1.setNumList(Arrays.asList("6", "2", "4", "0"));
-        game.guess(guessAnswer1);
+        game.guess(Answer.createAnswer("6 2 4 0"));
         assertEquals("continue", game.checkStatus());
         assertTrue(game.checkCoutinue());
 
-        Answer guessAnswer2 = new Answer();
-        guessAnswer2.setNumList(Arrays.asList("9", "2", "5", "4"));
-        game.guess(guessAnswer2);
+        game.guess(Answer.createAnswer("9 2 5 4"));
         assertEquals("continue", game.checkStatus());
         assertTrue(game.checkCoutinue());
 
-        Answer guessAnswer3 = new Answer();
-        guessAnswer3.setNumList(Arrays.asList("1", "3", "2", "4"));
-        game.guess(guessAnswer3);
+        game.guess(Answer.createAnswer("1 3 2 4"));
         assertEquals("continue", game.checkStatus());
+        assertTrue(game.checkCoutinue());
 
-        Answer guessAnswer4 = new Answer();
-        guessAnswer4.setNumList(Arrays.asList("1", "2", "3", "4"));
-        game.guess(guessAnswer4);
+        game.guess(Answer.createAnswer("1 2 3 4"));
         assertEquals("success", game.checkStatus());
         assertFalse(game.checkCoutinue());
     }
@@ -126,34 +96,22 @@ public class GameTest {
     @Test
     public void should_return_status_fail() {
 
-        Answer guessAnswer1 = new Answer();
-        guessAnswer1.setNumList(Arrays.asList("6", "2", "4", "0"));
-        game.guess(guessAnswer1);
+        game.guess(Answer.createAnswer("6 2 4 0"));
         assertEquals("continue", game.checkStatus());
 
-        Answer guessAnswer2 = new Answer();
-        guessAnswer2.setNumList(Arrays.asList("9", "2", "5", "4"));
-        game.guess(guessAnswer2);
+        game.guess(Answer.createAnswer("9 2 5 4"));
         assertEquals("continue", game.checkStatus());
 
-        Answer guessAnswer3 = new Answer();
-        guessAnswer3.setNumList(Arrays.asList("9", "7", "6", "0"));
-        game.guess(guessAnswer3);
+        game.guess(Answer.createAnswer("9 7 6 0"));
         assertEquals("continue", game.checkStatus());
 
-        Answer guessAnswer4 = new Answer();
-        guessAnswer4.setNumList(Arrays.asList("0", "2", "8", "1"));
-        game.guess(guessAnswer4);
+        game.guess(Answer.createAnswer("0 2 8 1"));
         assertEquals("continue", game.checkStatus());
 
-        Answer guessAnswer5 = new Answer();
-        guessAnswer5.setNumList(Arrays.asList("1", "2", "6", "3"));
-        game.guess(guessAnswer5);
+        game.guess(Answer.createAnswer("1 2 6 3"));
         assertEquals("continue", game.checkStatus());
 
-        Answer guessAnswer6 = new Answer();
-        guessAnswer6.setNumList(Arrays.asList("1", "2", "4", "3"));
-        game.guess(guessAnswer6);
+        game.guess(Answer.createAnswer("1 2 5 4"));
         assertEquals("fail", game.checkStatus());
         assertFalse(game.checkCoutinue());
     }
